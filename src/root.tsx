d828afeb2f032c6c0c69cf0c01cf1263a40a428c
Naveen Kumarasinghe <dndkumarasinghe@gmail.com>
2023-05-30 21:43:05 +0530

    feat(boilerplate): add theme configurations

--- src/root.tsx
+++ src/root.tsx
@@ -11,7 +11,7 @@ import {
     ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-import "./global.css";
+import "./global.scss";
 
 export default component$(() => {
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                highlight: "var(--color-highlight)",
+                accent: {
+                    DEFAULT: "var(--color-accent)",
+                    50: "var(--color-accent-50)",
+                    foreground: "var(--color-accent-foreground)",
+                },
+                base: {
+                    DEFAULT: "var(--color-base)",
+                    50: "var(--color-base-50)",
+                    100: "var(--color-base-100)",
+                    200: "var(--color-base-200)",
+                    300: "var(--color-base-300)",
+                    400: "var(--color-base-400)",
+                    500: "var(--color-base-500)",
+                    600: "var(--color-base-600)",
+                    700: "var(--color-base-700)",
+                    800: "var(--color-base-800)",
+                    900: "var(--color-base-900)",
+                    950: "var(--color-base-950)",
+                    foreground: "var(--color-base-foreground)",
+                },
+            },
+        },
     },
     plugins: [],
 };
