f7e8610df541a2247356726ba44962a116a38e3e
Naveen Kumarasinghe <dndkumarasinghe@gmail.com>
2023-06-12 00:02:42 +0530

    sign in 2

--- src/routes/signin/index.tsx
+++ src/routes/signin/index.tsx
@@ -1,21 +1,43 @@
 import { component$, $ } from "@builder.io/qwik";
 import SignUpForm from "./signin-form";
-import { routeAction$ } from "@builder.io/qwik-city";
+import { routeAction$, useNavigate } from "@builder.io/qwik-city";
+import AuthService from "~/services/auth-service";
 
-export const useSignIn = routeAction$(async (data, request) => {
+export const useSignIn = routeAction$(async (data: any, request) => {
+    const { email, password } = data;
+
+    const { status, accessToken } = await AuthService.signin(email, password);
+
+    // sign in success
+    if (status === "success") {
+        // store the JWT access token in user's browser's cookies
+        request.cookie.set("access_token", accessToken, {
+            httpOnly: true,
+            sameSite: true,
+            secure: true,
+        });
+        return true;
+    } else if (status === "fail") {
+        return false;
+    }
+
     request.redirect(302, "http://google.com");
 });
 
 export default component$(() => {
     const signIn = useSignIn();
+    const navigate = useNavigate();
+
+    const handleSubmit = $(async (formData) => {
+        const signInResult = await signIn.submit(formData);
+        if (signInResult) {
+            await navigate("/");
+        }
+    });
 
     return (
         <div class="flex h-[80vh] w-full items-center justify-center">
-            <SignUpForm
-                onSubmit$={(formData) => {
-                    signIn.submit(formData);
-                }}
-            />
+            <SignUpForm onSubmit$={handleSubmit} />
         </div>
     );
 });
--- src/services/auth-service/auth-service.tsx
+++ src/services/auth-service/auth-service.tsx
@@ -1,5 +1,5 @@
 export default abstract class AuthService {
-    static signin(username: string, password: string) {
+    static async signin(username: string, password: string) {
         // TODO: validate user credentials
         return {
             status: "success",
